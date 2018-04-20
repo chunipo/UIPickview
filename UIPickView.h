13c277ae962d50287070409e36a523450db0ddc3
weiyuxiang <weiyuxiang@talkmoney.cn>
2018-04-20 18:03:11 +0800

    一个可以展现至多3列的UIPickView，自定义度高

--- UIPickView.h
+++ UIPickView.h
@@ -1,9 +1,9 @@
 //
 //  QTMCarRegionPickerView.h
-//  QianTuMei
+//  
 //
 //  Created by weiyuxiang on 2018/4/18.
-//  Copyright © 2018年 luwei. All rights reserved.
+//
 //
 
 #import <UIKit/UIKit.h>
